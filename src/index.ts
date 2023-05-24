2b24da9031ee2a70f043f053aa118af3f34984af
Long Trinh <psatrinhlongat48@gmail.com>
2023-05-25 00:20:09 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import typeDefs from "./schema/book";
+import resolvers from "./resolver/book";
+
+async function main() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  app.use(cors(), bodyParser.json(), expressMiddleware(server));
+
+  await new Promise(() => httpServer.listen({ port: 4000 }));
+  console.log(`🚀 Server ready at http://localhost:4000`);
+}
+
+main();
